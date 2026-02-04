c9b559416e55449c968889fd5b1a692dad50a6cf
sanjay-1718 <sanjaybhargav1718@gmail.com>
2026-02-04 13:39:13 +0530

    Update Postman_Collection and made some minor changes

--- src/main/java/in/project/expensetracker/security/SecurityConfig.java
+++ src/main/java/in/project/expensetracker/security/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
